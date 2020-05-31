8b1f76b34a5c8ad45025fa23972a70db30de32ae
srimant <srimant.sahu@gmail.com>
2020-05-31 11:10:44 +0530

    optimized k closest points problem

--- src/monthlychallenege/may20/KClosestPoints.java
+++ src/monthlychallenege/may20/KClosestPoints.java
@@ -11,7 +11,7 @@ class KClosestPoints {
     }
 
     public int[][] kClosest(int[][] points, int K) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> Double.compare(Math.sqrt(a2[0] * a2[0] + a2[1] * a2[1]), Math.sqrt(a1[0] * a1[0] + a1[1] * a1[1])));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> Integer.compare(a2[0] * a2[0] + a2[1] * a2[1], a1[0] * a1[0] + a1[1] * a1[1]));
 
         for (int i = 0; i < K; i++) {
             pq.add(points[i]);
@@ -21,7 +21,7 @@ class KClosestPoints {
             int[] curr = points[i];
             int[] top = pq.peek();
 
-            if (Math.sqrt(curr[0] * curr[0] + curr[1] * curr[1]) < Math.sqrt(top[0] * top[0] + top[1] * top[1])) {
+            if (curr[0] * curr[0] + curr[1] * curr[1] < top[0] * top[0] + top[1] * top[1]) {
                 pq.remove();
                 pq.add(curr);
             }
