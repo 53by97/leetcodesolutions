ab42fafb1eb0073c231466aebd96d77642dd5277
srimant <srimant.sahu@gmail.com>
2020-06-01 13:15:17 +0530

    solved invert a tree problem

--- src/monthlychallenege/may20/CousinsInATree.java
+++ src/monthlychallenege/may20/CousinsInATree.java
@@ -34,9 +34,7 @@ class ParentDepth {
 class CousinsInATreeCousinNodes {
 
     public static void main(String[] args) {
-
         TreeNode root = new TreeNode(1, new TreeNode(2, null, new TreeNode(4)), new TreeNode(3, new TreeNode(5, new TreeNode(7, null, null), null), new TreeNode(6, new TreeNode(8, null, new TreeNode(9)), null)));
-
 
         System.out.println(isCousins(root, 1, 9));
     }
