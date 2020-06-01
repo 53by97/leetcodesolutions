df0b795cac8b05269ffa7c389d477a2b962d0962
srimant <srimant.sahu@gmail.com>
2020-06-01 21:57:21 +0530

    optimized edit distance problem with 2 arrays

--- src/monthlychallenege/may20/EditDistance.java
+++ src/monthlychallenege/may20/EditDistance.java
@@ -11,23 +11,25 @@ class EditDistance {
     }
 
     private int editDistance(String word1, String word2, int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
-
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0) {
-                    dp[i][j] = j;
-                } else if (j == 0) {
-                    dp[i][j] = i;
-                } else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1]));
-                }
-            }
+        int[] dp0 = new int[n + 1];
+        for (int j = 0; j <= n; j++) {
+            dp0[j] = j;
         }
 
-        return dp[m][n];
+        for (int i = 1; i <= m; i++) {
+            int[] dp1 = new int[n + 1];
+            dp1[0] = i;
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp1[j] = dp0[j - 1];
+                } else {
+                    dp1[j] = 1 + Math.min(dp1[j - 1], Math.min(dp0[j], dp0[j - 1]));
+                }
+            }
+            dp0 = dp1;
+        }
+
+        return dp0[n];
     }
 
 }
